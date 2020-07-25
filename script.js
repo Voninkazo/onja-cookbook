3b4b91d7329b6fc0eea49823e744e1a326ef021d
Sandy Raveloarisoa <sandy.rav@onja.org>
2020-07-25 10:31:42 +0300

    modified html

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log('it works');
+
+// handle input
+const recepieName = document.querySelector('#recepie-name');
+const cookName = document.querySelector('#cook-name');
+const difficulty = document.querySelector('#difficulty');
+const duration = document.querySelector('#cooking-time');
+
