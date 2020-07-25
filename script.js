780e537de73b3e96468d2c903c80093c20bd6218
Sandy Raveloarisoa <sandy.rav@onja.org>
2020-07-25 11:02:28 +0300

    I added placholder attributes in the name, cook and url input, then aded wraper element for the step list and the ingredient lists. Also added event delegation to for a click on the add step button

--- script.js
+++ script.js
@@ -6,3 +6,21 @@ const cookName = document.querySelector('#cook-name');
 const difficulty = document.querySelector('#difficulty');
 const duration = document.querySelector('#cooking-time');
 
+
+window.addEventListener('click', event => {
+  if(event.target.matches('.add-ingredient')) {
+    const myhtml =`
+    <input type="text" name="input-step" id="input-step" required>
+    `;
+    const ingredientLists = document.querySelector('.igredient-lists');
+    ingredientLists.insertAdjacentHTML('beforeend', myhtml);
+  }
+
+  if (event.target.matches('.add-steps')) {
+    const myHtml = `
+    <input type="text" name="input-step" id="input-step" required>
+    `;
+    const stepLists = document.querySelector('#step');
+    stepLists.insertAdjacentHTML('afterbegin', myHtml);
+  }
+});
